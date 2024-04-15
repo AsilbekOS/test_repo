b160d9ef02f5f5937106fffa1ab0cb879b6ac710
AsilbekOS <asilbekxolmatov2002@gmail.com>
2024-04-15 10:57:10 +0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AsilbekOS/test_repo
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/AsilbekOS/test_repo/greeting"
+)
+
+func main() {
+	fmt.Println(greeting.Greet())
+}
